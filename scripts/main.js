48ad7167fd299f8882733643e3c42e7b40d97aa0
Anthony Mackensen <tonymckes@gmail.com>
2024-07-03 14:12:36 -0400

    feat: add language toggler

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 const toggle = document.querySelector(".toggler");
-
 toggle.addEventListener("click", () => {
   toggle.ariaExpanded = toggle.ariaExpanded !== "true";
 });
+
+// TODO: Detect client's language
+document.querySelector(".toggle-lang").addEventListener("click", () => {
+  const lang = document.documentElement.getAttribute("lang");
+  const dir = document.documentElement.getAttribute("dir");
+
+  if (lang === "en" && dir === "ltr") {
+    document.documentElement.setAttribute("lang", "he");
+    document.documentElement.setAttribute("dir", "rtl");
+  } else {
+    document.documentElement.setAttribute("lang", "en");
+    document.documentElement.setAttribute("dir", "ltr");
+  }
+});
+
